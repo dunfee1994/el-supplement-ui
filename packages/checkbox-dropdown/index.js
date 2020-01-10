2394dea876d7f4c81ea9eac3f77839e5c5b944ec
dunfee1994 <2356648032@qq.com>
2020-01-10 17:55:17 +0800

    checkbox-dropdown

---
+++ packages/checkbox-dropdown/index.js
@@ -0,0 +1,8 @@
+import ElSupCheckboxDropDown from './src/Main.vue';
+
+/* istanbul ignore next */
+ElSupCheckboxDropDown.install = function(vue) {
+    Vue.component(ElSupCheckboxDropDown.name, ElSupCheckboxDropDown);
+};
+
+export default ElSupCheckboxDropDown;
---
+++ packages/checkbox-dropdown/src/Main.vue
@@ -0,0 +1,55 @@
+<template>
+    <el-dropdown :hide-on-click="false">
+        <el-button>
+            列表项
+            <i class="el-icon-arrow-down el-icon--right"></i>
+        </el-button>
+        <el-dropdown-menu
+            slot="dropdown"
+            class="el-sup-dropdown-checkbox"
+            :style="`max-height:${maxHeight}`"
+        >
+            <el-checkbox-group v-model="checkedColumns" @change="handleChange">
+                <el-dropdown-item v-for="(item,index) in allColumns" :key="index">
+                    <el-checkbox :label="item.label">{{item.name}}</el-checkbox>
+                </el-dropdown-item>
+            </el-checkbox-group>
+        </el-dropdown-menu>
+    </el-dropdown>
+</template>
+<script>
+/**
+ * @description 下拉复选框
+ * @author dunfee
+ * @date 2020/01/10
+ */
+export default {
+    name: "ElSupCheckboxDropDown",
+    props: {
+        maxHeight: {
+            type: String,
+            default: () => "290px"
+        },
+        value: Array,
+        allColumns: {
+            type: Array,
+            default: () => []
+        }
+    },
+    data() {
+        return {
+            checkedColumns: []
+        };
+    },
+    created() {
+        this.checkedColumns = JSON.parse(JSON.stringify(this.value));
+    },
+    methods: {
+        handleChange(val) {
+            // 处理change事件
+            this.$emit("input", val);
+            this.$emit("change", val);
+        }
+    }
+};
+</script>
